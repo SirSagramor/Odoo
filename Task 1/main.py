8eaa0e23a377b4d0f096f48701d5a9029c7b37ca
SirSagramor <sir.sagramor@gmail.com>
2018-07-02 20:31:12 +0300

    Simply changed task 1

--- Task 1/main.py
+++ Task 1/main.py
@@ -3,6 +3,11 @@
     Створити функцію що приймає число, перевіряє його та виводить “Even” або “Odd”
 """
 
-
+# parity check function
 def oddOrEven(num):
     return "Odd" if num % 2 else "Even"
+
+# Odd
+print(oddOrEven(5))
+# Even
+print(oddOrEven(142))
